c11a18fd72a1da4084fb738635c45d03aff59825
Jesus <150912607+Prometheus1993@users.noreply.github.com>
2024-02-26 00:35:30 +0000

    aanpassing anime-cg

--- heroData/anime-cg.ts
+++ heroData/anime-cg.ts
@@ -1,14 +1,35 @@
-import readline from 'readline-sync';
+import readline from "readline-sync";
+import { readFileSync } from "fs";
+import { join } from "path";
+import { Character, Power } from "./interface";
 
-import { Character, Power } from "../heroData/interface";
+let stop: boolean = false;
 
+const viewAllData = () => {};
 
-//keuzemenu
+const filterById = () => {
+  const id = readline.questionInt("Voer een ID in: ");
+};
 
-console.log("Welkom bij de Anime-Cardgame Data Viewer!!\n");
+// Keuzemenu
+while (!stop) {
+  console.log("\nWelkom bij de Anime-Cardgame Data Viewer!!\n");
+  console.log("1. Bekijk alle data\n2. Filter op ID\n3. Exit\n");
 
-console.log("1. Bekijk alle data\n2. Filter op ID\n3. Exit\n");
-
-let choice: number = readline.questionInt("maak je keuze: ");
-
-export {}
+  let choice: number = readline.questionInt("Maak je keuze: ");
+  switch (choice) {
+    case 1:
+      viewAllData();
+      break;
+    case 2:
+      filterById();
+      break;
+    case 3:
+      stop = true;
+      break;
+    default:
+      console.log("Ongeldige keuze, probeer het opnieuw.");
+      break;
+  }
+}
+console.log("\nTot ziens!");
